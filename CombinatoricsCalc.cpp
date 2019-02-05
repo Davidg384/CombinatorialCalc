e862645a6c4d404e0fbf08d2a436f1a021129d60
davidg384 <davidg384@gmail.com>
2019-02-05 14:30:31 -0500

    added extra white line after calculation

--- CombinatoricsCalc.cpp
+++ CombinatoricsCalc.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main(){
 	int choice, n, k;
-	cout << "Combinatorics Calculator v.1.0.0" << endl << endl;
+	cout << "Combinatorics Calculator v.1.1.0" << endl << endl;
 	while(true){
 		cout << "[0] Factorial \n[1] Permutation \n[2] Combination \n[3] Multicombination \n[4] Exit" << endl;
 		cin >> choice;
 		if(choice == 0){
 			cout << "n!" << endl << "What is your n? ";
 			cin >> n;
-			cout << endl << n << "! = " << permute(n,n) << endl;
+			cout << endl << n << "! = " << permute(n,n) << endl << endl;
 		}
 		else if(choice == 1){
 			cout << "n P k" << endl;
@@ -20,7 +20,7 @@ int main(){
 			cin >> n;
 			cout << "\nWhat is your k? ";
 			cin >> k;
-			cout << n << " P " << k << " = " << permute(n, k) << endl;
+			cout << n << " P " << k << " = " << permute(n, k) << endl << endl;
 		}
 		else if(choice == 2){
 			cout << "n choose k" << endl;
@@ -28,7 +28,7 @@ int main(){
 			cin >> n;
 			cout << "\nWhat is your k? ";
 			cin >> k;
-			cout << n << " choose " << k << " = " << choose(n, k) << endl;
+			cout << n << " choose " << k << " = " << choose(n, k) << endl << endl;
 		}
 		else if(choice == 3){
 			cout << "n multichoose k" << endl;
@@ -36,7 +36,7 @@ int main(){
 			cin >> n;
 			cout << "\nWhat is your k? ";
 			cin >> k;
-			cout << n << " P " << k << " = " << multichoose(n, k) << endl;
+			cout << n << " P " << k << " = " << multichoose(n, k) << endl << endl;
 		}
 		else if(choice == 4){
 			cout << "Exiting . . ." << endl;
